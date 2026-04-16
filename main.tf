1138ede3661bd1fcfb6c923f2e48183a9c3e9dd4
Ibrahim Omar <abra56heem@gmail.com>
2026-04-16 20:30:35 +0200

    Adding an Azure app service plan

--- main.tf
+++ main.tf
@@ -35,3 +35,17 @@ module "keyvault" {
   }
 }
 
+module "service_plan" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=service_plan/v2.0.0"
+  # also any inputs for the module (see below)
+  app_service_plan_name = "ibrahimplan1234"
+  resource_group = {
+    name     = "rg-user6"
+    location = "northeurope"  
+  }
+  sku_name = "B1"
+  tags = {
+    environment = "production"
+    project     = "myapp"
+  }
+}
